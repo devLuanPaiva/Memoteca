05b150c88fa91daa2d0489645f35e0722ba27319
Luan Alves <teste.checklist@nossoatacarejo.com.br>
2025-02-05 16:24:59 -0300

    feat:  import footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
