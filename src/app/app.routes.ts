69cb27ab5e46017247f1881ee15dc4b35864239d
dev Luan Paiva <devluanpaiva@gmail.com>
2025-02-06 15:01:18 -0300

    feat: create path to  thought form page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ThoughtFormComponent } from './components/thought/thought-form/thought-form.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'criar',
+    component: ThoughtFormComponent
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
