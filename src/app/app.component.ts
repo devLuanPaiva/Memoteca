8c992afb705bc37786a90a0e7b3aa18c21eabf1b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-02-09 11:09:17 -0300

    refactor: remove title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./components/header/header.component";
-import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, RouterModule],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'memoteca';
 }
