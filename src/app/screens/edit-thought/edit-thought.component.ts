337358eddab40635ddb831f6d7e429beeb475011
dev Luan Paiva <devluanpaiva@gmail.com>
2025-02-13 07:32:16 -0300

    refactor: implement validators

--- src/app/screens/edit-thought/edit-thought.component.ts
+++ src/app/screens/edit-thought/edit-thought.component.ts
@@ -3,41 +3,56 @@ import { Thinking } from '../../interfaces/thinking';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ThinkingService } from '../../services/thinking.service';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormsModule, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-thought',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './edit-thought.component.html',
 })
 export class EditThoughtComponent implements OnInit {
-  thinking: Thinking = {
-    id: 0,
-    auth: '',
-    content: '',
-    model: ''
-  }
+  form!: FormGroup
 
   constructor(
     private readonly thoughtService: ThinkingService,
     private readonly activatedRoute: ActivatedRoute,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['id'];
     if (id) {
       this.thoughtService.findById(parseInt(id)).subscribe((thinking: Thinking) => {
-        this.thinking = thinking;
+        this.form = this.formBuilder.group({
+          id: [thinking.id],
+          content: [thinking.content, Validators.compose([
+            Validators.required,
+            Validators.pattern(/(.|\s)*\S(.|\s)*/)
+          ])],
+          auth: [thinking.auth, Validators.compose([
+            Validators.required,
+            Validators.minLength(3)
+          ])],
+          model: [thinking.model, Validators.compose([
+            Validators.required
+          ])]
+        })
       })
     }
   }
   editThink() {
-    this.thoughtService.edit(this.thinking).subscribe(() => {
+    this.thoughtService.edit(this.form.value).subscribe(() => {
       this.router.navigate(['/']);
     });
   }
   cancel() {
     this.router.navigate(['/']);
+  }
+  enableButton(): string{
+    if(this.form.invalid){
+      return 'disabled'
+    }
+    return ''
   }
 }
