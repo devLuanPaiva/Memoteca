3c3110c6176e8757ca1b409c47c36730f1947e5a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-02-13 20:40:21 -0300

    refactor: implement loadMoreThoughts function

--- src/app/screens/list-thought/list-thought.component.ts
+++ src/app/screens/list-thought/list-thought.component.ts
@@ -6,19 +6,31 @@ import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCommentSlash } from '@fortawesome/free-solid-svg-icons';
 import { ThoughtComponent } from '../../components/thought/thought/thought.component';
+import { LoadMoreButtonComponent } from "../../components/shared/load-more-button/load-more-button.component";
 
 @Component({
   selector: 'app-list-thought',
-  imports: [CommonModule, ThoughtComponent, RouterModule, FontAwesomeModule],
+  imports: [CommonModule, ThoughtComponent, RouterModule, FontAwesomeModule, LoadMoreButtonComponent],
   templateUrl: './list-thought.component.html',
 })
 export class ListThoughtComponent implements OnInit {
-  listThought: Thinking[] = []
-  constructor(private readonly service: ThinkingService) { }
-  commentSplash = faCommentSlash
+  listThought: Thinking[] = [];
+  currentPage: number = 1;
+  haveMoreThought: boolean = true;
+  commentSplash = faCommentSlash;
+
+  constructor(private readonly service: ThinkingService) {}
   ngOnInit(): void {
-    this.service.list().subscribe((listThought) => {
-      this.listThought = listThought
-    })
+    this.service.list(this.currentPage).subscribe((listThought) => {
+      this.listThought = listThought;
+    });
+  }
+  loadMoreThoughts(): void {
+    this.service.list(++this.currentPage).subscribe((listThought) => {
+      this.listThought.push(...listThought);
+      if (!listThought.length) {
+        this.haveMoreThought = false;
+      }
+    });
   }
 }
