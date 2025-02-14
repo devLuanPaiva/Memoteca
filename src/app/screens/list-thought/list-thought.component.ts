f4c2762e60a28b21d8e7f227edaa3b9924e169d3
dev Luan Paiva <devluanpaiva@gmail.com>
2025-02-14 07:52:30 -0300

    feat: implements filterThoughts function

--- src/app/screens/list-thought/list-thought.component.ts
+++ src/app/screens/list-thought/list-thought.component.ts
@@ -4,33 +4,46 @@ import { ThinkingService } from '../../services/thinking.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCommentSlash } from '@fortawesome/free-solid-svg-icons';
+import { faCommentSlash, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { ThoughtComponent } from '../../components/thought/thought.component';
 import { LoadMoreButtonComponent } from "../../components/shared/load-more-button/load-more-button.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-thought',
-  imports: [CommonModule, ThoughtComponent, RouterModule, FontAwesomeModule, LoadMoreButtonComponent],
+  imports: [CommonModule, FormsModule, ThoughtComponent, RouterModule, FontAwesomeModule, LoadMoreButtonComponent],
   templateUrl: './list-thought.component.html',
 })
 export class ListThoughtComponent implements OnInit {
   listThought: Thinking[] = [];
   currentPage: number = 1;
   haveMoreThought: boolean = true;
-  commentSplash = faCommentSlash;
+  filter: string = ''
+  icons = {
+    filter: faMagnifyingGlass,
+    commentSplash: faCommentSlash
+  }
 
-  constructor(private readonly service: ThinkingService) {}
+  constructor(private readonly service: ThinkingService) { }
   ngOnInit(): void {
-    this.service.list(this.currentPage).subscribe((listThought) => {
+    this.service.list(this.currentPage, this.filter).subscribe((listThought) => {
       this.listThought = listThought;
     });
   }
   loadMoreThoughts(): void {
-    this.service.list(++this.currentPage).subscribe((listThought) => {
+    this.service.list(++this.currentPage, this.filter).subscribe((listThought) => {
       this.listThought.push(...listThought);
       if (!listThought.length) {
         this.haveMoreThought = false;
       }
+    });
+  }
+  filterThoughts(): void {
+    this.haveMoreThought = true;
+    this.currentPage = 1;
+    this.listThought = [];
+    this.service.list(this.currentPage, this.filter).subscribe((listThought) => {
+      this.listThought = listThought;
     });
   }
 }
